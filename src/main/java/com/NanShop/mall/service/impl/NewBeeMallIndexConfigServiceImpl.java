ac35ab7afedd159d014b294a9a5259fd71ea02dd
Aristochi <47059083+Aristochi@users.noreply.github.com>
2020-07-17 20:53:32 +0800

    first commit

---
+++ src/main/java/com/NanShop/mall/service/impl/NewBeeMallIndexConfigServiceImpl.java
@@ -0,0 +1,107 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2019-2020 十三 all rights reserved.
+ * 版权所有，侵权必究！
+ */
+package com.NanShop.mall.service.impl;
+
+import com.NanShop.mall.common.ServiceResultEnum;
+import com.NanShop.mall.controller.vo.NewBeeMallIndexConfigGoodsVO;
+import com.NanShop.mall.dao.IndexConfigMapper;
+import com.NanShop.mall.dao.NewBeeMallGoodsMapper;
+import com.NanShop.mall.util.BeanUtil;
+import com.NanShop.mall.util.PageQueryUtil;
+import com.NanShop.mall.util.PageResult;
+import com.NanShop.mall.entity.IndexConfig;
+import com.NanShop.mall.entity.NewBeeMallGoods;
+import com.NanShop.mall.service.NewBeeMallIndexConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class NewBeeMallIndexConfigServiceImpl implements NewBeeMallIndexConfigService {
+
+    @Autowired
+    private IndexConfigMapper indexConfigMapper;
+
+    @Autowired
+    private NewBeeMallGoodsMapper goodsMapper;
+
+    @Override
+    public PageResult getConfigsPage(PageQueryUtil pageUtil) {
+        List<IndexConfig> indexConfigs = indexConfigMapper.findIndexConfigList(pageUtil);
+        int total = indexConfigMapper.getTotalIndexConfigs(pageUtil);
+        PageResult pageResult = new PageResult(indexConfigs, total, pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
+
+    @Override
+    public String saveIndexConfig(IndexConfig indexConfig) {
+        //todo 判断是否存在该商品
+        if (indexConfigMapper.insertSelective(indexConfig) > 0) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.DB_ERROR.getResult();
+    }
+
+    @Override
+    public String updateIndexConfig(IndexConfig indexConfig) {
+        //todo 判断是否存在该商品
+        IndexConfig temp = indexConfigMapper.selectByPrimaryKey(indexConfig.getConfigId());
+        if (temp == null) {
+            return ServiceResultEnum.DATA_NOT_EXIST.getResult();
+        }
+        if (indexConfigMapper.updateByPrimaryKeySelective(indexConfig) > 0) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.DB_ERROR.getResult();
+    }
+
+    @Override
+    public IndexConfig getIndexConfigById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<NewBeeMallIndexConfigGoodsVO> getConfigGoodsesForIndex(int configType, int number) {
+        List<NewBeeMallIndexConfigGoodsVO> newBeeMallIndexConfigGoodsVOS = new ArrayList<>(number);
+        List<IndexConfig> indexConfigs = indexConfigMapper.findIndexConfigsByTypeAndNum(configType, number);
+        if (!CollectionUtils.isEmpty(indexConfigs)) {
+            //取出所有的goodsId
+            List<Long> goodsIds = indexConfigs.stream().map(IndexConfig::getGoodsId).collect(Collectors.toList());
+            List<NewBeeMallGoods> newBeeMallGoods = goodsMapper.selectByPrimaryKeys(goodsIds);
+            newBeeMallIndexConfigGoodsVOS = BeanUtil.copyList(newBeeMallGoods, NewBeeMallIndexConfigGoodsVO.class);
+            for (NewBeeMallIndexConfigGoodsVO newBeeMallIndexConfigGoodsVO : newBeeMallIndexConfigGoodsVOS) {
+                String goodsName = newBeeMallIndexConfigGoodsVO.getGoodsName();
+                String goodsIntro = newBeeMallIndexConfigGoodsVO.getGoodsIntro();
+                // 字符串过长导致文字超出的问题
+                if (goodsName.length() > 30) {
+                    goodsName = goodsName.substring(0, 30) + "...";
+                    newBeeMallIndexConfigGoodsVO.setGoodsName(goodsName);
+                }
+                if (goodsIntro.length() > 22) {
+                    goodsIntro = goodsIntro.substring(0, 22) + "...";
+                    newBeeMallIndexConfigGoodsVO.setGoodsIntro(goodsIntro);
+                }
+            }
+        }
+        return newBeeMallIndexConfigGoodsVOS;
+    }
+
+    @Override
+    public Boolean deleteBatch(Long[] ids) {
+        if (ids.length < 1) {
+            return false;
+        }
+        //删除数据
+        return indexConfigMapper.deleteBatch(ids) > 0;
+    }
+}
