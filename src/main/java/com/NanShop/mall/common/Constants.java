6a53d534f16e6c31fa5b7a629a0d110d437c36de
Aristochi <47059083+Aristochi@users.noreply.github.com>
2020-07-26 18:13:24 +0800

    修改了部分细节展示

--- src/main/java/com/NanShop/mall/common/Constants.java
+++ src/main/java/com/NanShop/mall/common/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     //public final static String FILE_UPLOAD_DIC = "/opt/image/upload/";//上传文件的默认url前缀，根据部署设置自行修改
     public final static String FILE_UPLOAD_DIC = "H:\\MavenDemo\\Nanshop-mall-master\\src\\main\\resources\\upload\\";//上传文件的默认url前缀，根据部署设置自行修改
 
-    public final static int INDEX_CAROUSEL_NUMBER = 5;//首页轮播图数量(可根据自身需求修改)
+    public final static int INDEX_CAROUSEL_NUMBER = 6;//首页轮播图数量(可根据自身需求修改)
 
     public final static int INDEX_CATEGORY_NUMBER = 11;//首页一级分类的最大数量
 
